7345f0dc5c947ff2813b9badd2f18de2eb22a1fe
bmoreira <bmoreira@student.42sp.org.br>
2025-09-30 03:26:14 -0300

    adjust in push_a and push_b

--- src/push.c
+++ src/push.c
@@ -6,26 +6,26 @@
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 22:07:58 by bmoreira          #+#    #+#             */
-/*   Updated: 2025/09/30 03:15:57 by bmoreira         ###   ########.fr       */
+/*   Updated: 2025/09/30 03:25:39 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_node	*push(t_node *first, t_node *second)
-{
-	ft_lstadd_front(&first, second);
-	lst_del_front(&second, free);
-}
-
 void	*push_a(t_list *a, t_list *b)
 {
+	if (!b)
+		return ;
 	ft_lstadd_front(&a, b);
 	lst_del_front(&b, free);
+	ft_printf("pa");
 }
 
 void	*push_b(t_list *a, t_list *b)
 {
+	if (!a)
+		return ;
 	ft_lstadd_front(&b, a);
 	lst_del_front(&a, free);
+	ft_printf("pb");
 }
