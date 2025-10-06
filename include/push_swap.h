abd479ea9b38f85a1ba0d9ed27b7dcebe158fb69
bmoreira <bmoreira@student.42sp.org.br>
2025-10-06 18:54:14 -0300

    added swap operation

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 19:27:47 by bmoreira          #+#    #+#             */
-/*   Updated: 2025/10/06 18:45:34 by bmoreira         ###   ########.fr       */
+/*   Updated: 2025/10/06 18:52:49 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@
 void	push(t_list **head, t_list **node);
 void	rotate(t_list **head);
 void	reverse_rotate(t_list **head);
+void	swap(t_list **head);
 
 #endif
--- src/swap.c
+++ src/swap.c
@@ -6,20 +6,48 @@
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 19:39:15 by bmoreira          #+#    #+#             */
-/*   Updated: 2025/09/30 03:32:48 by bmoreira         ###   ########.fr       */
+/*   Updated: 2025/10/06 18:53:48 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-t_list	*swap(t_list *head)
+void	swap(t_list **head)
 {
 	void	*temp;
 
-	if (!head || !head->next)
-		return (head);
-	temp = head->content;
-	head->content = head->next->content;
-	head->next->content = temp;
-	return (head);
+	if (!head || !*head)
+		return ;
+	temp = (*head)->content;
+	(*head)->content = (*head)->next->content;
+	(*head)->next->content = temp;
 }
+
+// int main(void)
+// {
+//     // Test with ints
+// 	t_list *head = NULL;
+// 	int vals[] = {9, 3, 1, 2, 4};
+// 	int i = 0;
+//
+// 	while (i < 5)
+// 		lst_add_back(&head, lst_new(&vals[i++]));
+//
+// 	ft_printf("Before: ");
+// 	lst_iter(head, lst_print_int);
+// 	swap(&head);
+// 	ft_printf("\nAfter1: ");
+// 	lst_iter(head, lst_print_int);
+// 	swap(&head);
+// 	ft_printf("\nAfter2: ");
+// 	lst_iter(head, lst_print_int);
+// 	swap(&head);
+// 	ft_printf("\nAfter3: ");
+// 	lst_iter(head, lst_print_int);
+// 	swap(&head);
+// 	ft_printf("\nAfter4: ");
+// 	lst_iter(head, lst_print_int);
+// 	swap(&head);
+// 	ft_printf("\nAfter5: ");
+// 	lst_iter(head, lst_print_int);
+// }
