58cc2e733494943f382e287b81ded8e2e20dd9eb
bmoreira <bmoreira@student.42sp.org.br>
2025-10-11 23:31:57 -0300

    finished parsing args to list and created cleanup function to avoid memory leaks

--- src/main.c
+++ src/main.c
@@ -6,38 +6,25 @@
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 03:38:11 by bmoreira          #+#    #+#             */
-/*   Updated: 2025/10/10 17:00:15 by bmoreira         ###   ########.fr       */
+/*   Updated: 2025/10/11 23:29:40 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-// void	lst_append(t_list **head, char **args)
-// {
-// 	t_list	*node;
-// 	int		i;
-
-// 	while (*args[i])
-// 	{
-// 		node = lst_new(*args[i]);
-// 	}
-// }
-
-// void	parse_args2(char **args)
-// {
-// 	char	**split;
-// 	t_list	**head;
-
-// 	head = malloc(sizeof(t_list **));
-// 	while(**args++)
-// 	{
-// 		//split = malloc(sizeof(char *));
-// 		split = ft_split(*args, ' ');
-// 		lst_append(head, split);
-// 		ft_split_free(split);
-// 	}
-// }
-
+int	exit_cleanup(t_list **lst, char **args, int exit_type)
+{
+	if (lst)
+	{
+		lst_clear(lst, free);
+		free(lst);
+	}
+	if (args)
+		ft_split_free(args);
+	if (exit_type == EXIT_FAILURE)
+		ft_printf("Error\n");
+	return (exit_type);
+}
 
 char	**read_args(int argc, char **args)
 {
@@ -46,58 +33,51 @@ char	**read_args(int argc, char **args)
 	
 	i = 0;
 	if (argc == 1)
-		split = ft_split(*args, ' ');
+		return (ft_split(*args, ' '));
 	else
-	{
 		split = ft_calloc(argc + 1, sizeof(char *));
-		if (!split)
-			return (NULL);
-		while (i < argc)
-			split[i++] = *args++;
-	}
+	if (!split || !*split)
+		return (FALSE);
+	while (i < argc)
+		split[i++] = *args++;
 	return (split);
-}
-
-void	print_list_int(t_list *head)
-{
-	while(head)
-	{
-		ft_printf("%d\n", *(int *)head->content);
-		head = head->next;
-	}
 }
 
 int	parse_args(t_list **head, char **args)
 {
 	long	*num;
+	int		i;
 	
-	while (*args)
+	i = 0;
+	while (args[i])
 	{
-		num = malloc(1 * sizeof(long));
+		num = ft_calloc(1, sizeof(long));
 		if (!num)
-			return (0);
-		*num = ft_atol(*args++);
+			return (FALSE);
+		*num = ft_atol(args[i++]);
 		if (*num > INT_MAX || *num < INT_MIN)
-		{
-			lst_clear(head, free);
-			return (0);
-		}
+			return (free(num), FALSE);
 		else
 			lst_add_back(head, lst_new(num));
 	}
-	return (1);
+	return (TRUE);
 }
 // cc -Wall -Wextra -Werror main.c ../libft/src/str/* ../libft/src/mem/* ../libft/src/printf/* ../libft/src/math/* ../libft/src/char/* ../libft/src/lst/* && ./a.out "1 2 aaa   3  4  bada 2147483648" && rm a.out
 int	main(int argc, char **argv)
 {
 	char **args;
-	t_list *head = NULL;
+	t_list **head;
 	
 	if (argc > 1)
 	{
+		head = ft_calloc(1, sizeof(t_list *));
 		args = read_args(argc - 1, argv + 1);
-		parse_args(&head, args);
-		print_list_int(head);
+		if (!args)
+			return (exit_cleanup(NULL, NULL, EXIT_FAILURE));
+		if (!parse_args(head, args))
+			return (exit_cleanup(head, args, EXIT_FAILURE));
+		lst_print_int(*head);
+		return (exit_cleanup(head, args, EXIT_SUCCESS));
 	}
 }
 
