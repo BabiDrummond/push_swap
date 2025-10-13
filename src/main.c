7992b9f8de36ccdbe12d00c605429a0bfe736624
bmoreira <bmoreira@student.42sp.org.br>
2025-10-12 23:00:01 -0300

    added rules in makefile (for valgrind and gdb) for making testing easier. finished checking duplicated function

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 03:38:11 by bmoreira          #+#    #+#             */
-/*   Updated: 2025/10/12 19:24:53 by bmoreira         ###   ########.fr       */
+/*   Updated: 2025/10/12 21:24:36 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,23 @@ int	parse_args(t_list **head, char **args)
 	return (TRUE);
 }
 
-// cc -Wall -Wextra -Werror main.c ../libft/src/str/* ../libft/src/mem/* ../libft/src/printf/* ../libft/src/num/* ../libft/src/char/* ../libft/src/lst/* ../libft/src/sort/* && ./a.out "1 2 aaa   3  4  bada 2147483648" && rm a.out
+int	isequal_int(void *a, void *b)
+{
+	return (*(int *)a == *(int *)b);
+}
+
+int	check_duplicates(t_list *head)
+{
+	bubble_lst(head, cmp_int);
+	while (head->next)
+	{
+		if (isequal_int(head->content, head->next->content))
+			return (FALSE);
+		head = head->next;
+	}
+	return (TRUE);
+}
+
 int	main(int argc, char **argv)
 {
 	char **args;
@@ -73,11 +89,12 @@ int	main(int argc, char **argv)
 	{
 		head = ft_calloc(1, sizeof(t_list *));
 		args = read_args(argc - 1, argv + 1);
-		if (!args)
+		if (!args && ft_printf("Error reading args\n"))
 			return (exit_cleanup(NULL, NULL, EXIT_FAILURE));
-		if (!parse_args(head, args))
+		if (!parse_args(head, args) && ft_printf("Error parsing args\n"))
 			return (exit_cleanup(head, args, EXIT_FAILURE));
-		bubble_lst(*head, cmp_int);
+		if (!check_duplicates(*head) && ft_printf("Duplicated args\n"))
+			return (exit_cleanup(head, args, EXIT_FAILURE));
 		lst_print_int(*head);
 		return (exit_cleanup(head, args, EXIT_SUCCESS));
 	}
