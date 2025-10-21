98f39b6df6813f596503f9a1ac1828923b8898a3
babidrummond <babidrummond@hotmail.com>
2025-10-21 03:09:56 -0300

    nao sei mais oq fazer

--- src/sorting/sort.c
+++ src/sorting/sort.c
@@ -6,7 +6,7 @@
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 01:17:57 by bmoreira          #+#    #+#             */
-/*   Updated: 2025/10/21 02:58:53 by bmoreira         ###   ########.fr       */
+/*   Updated: 2025/10/21 03:05:03 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,45 +40,13 @@ void	sort_tree(t_stacks *stacks)
 	max = stack_size(i) - 1;
 	if (i->index == max && j->index < k->index)
 		ra(stacks);
-	while(!is_sorted(i))
+	while(!is_sorted_index(i))
 	{
 		if (i->index > j->index)
 			sa(stacks);
 		else if (j->index > k->index)
 			rra(stacks);
 	}
-	
-	0 2 1 j > k
-	rra 1 0 2 i > j
-	sa 0 1 2
-	
-	1 0 2 i > j
-	sa 0 1 2
-	
-	1 2 0 j > k
-	rra 0 1 2
-	
-	2 0 1 i = MAX & j < k
-	ra 0 1 2
-
-	2 1 0 i > j
-	sa 1 2 0  j > k
-	rra 0 1 2
-
-	
-	
-	t_stack	*stack;
-
-	stack = *stacks->a;
-	while (stack->next)
-	{
-		stack_print(stack);
-		if (stack->index > stack->next->index)
-			sa(stacks);
-		else
-			ra(stacks);
-		ra(stacks);
-	}		
 }
 
 // void	sort_tree(t_stacks *stacks)
